7ed0c4548e68f5a4f5d455640cb89c14dfb66065
Alimeow24 <98255072+Alimeow24@users.noreply.github.com>
2022-02-24 17:45:21 +0800

    Add files via upload

---
+++ week01-1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("輸入一個數值: ");
+    scanf("%d", &a);
+    printf("讀到了 %d\n", a);
+    return 0;
+}
---
+++ week01-2.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    char c;
+    printf("輸入一堆字: ");
+    scanf("%c", &c);
+    printf("讀到了 %c\n", c);
+    return 0;
+}
